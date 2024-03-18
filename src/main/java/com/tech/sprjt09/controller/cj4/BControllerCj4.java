8b4af7fde72287962c2aeffead003574ee2e2d2d
sacmkim <sacmkim@gmail.com>
2024-03-18 16:17:31 +0900

    BController 주석 해제

--- src/main/java/com/tech/sprjt09/controller/cj4/BControllerCj4.java
+++ src/main/java/com/tech/sprjt09/controller/cj4/BControllerCj4.java
@@ -23,9 +23,10 @@ public class BControllerCj4 {
 		System.out.println("delete()");
 ////		dbï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½
 //		//toss
-//		model.addAttribute("request",request);
-//		bServiceInter=new BDeleteService();
-//		bServiceInter.execute(model);
+		// ±è°æÅÂ ÁÖ¼® ÇØÁ¦
+		model.addAttribute("request",request);
+		bServiceInter=new BDeleteService();
+		bServiceInter.execute(model);
 //		
 		return "redirect:list";
 	}
